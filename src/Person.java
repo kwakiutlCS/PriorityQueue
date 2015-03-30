4543e14529f241e90107cbef4e687a6a9d891c1f
ricardo <ricardo.rsr.rodrigues@gmail.com>
2015-03-30 14:25:08 +0100

    initial commit

---
+++ src/Person.java
@@ -0,0 +1,53 @@
+import java.util.Comparator;
+
+/**
+ * Created by ricardo on 30/03/2015.
+ */
+public class Person implements Comparable<Person> {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof  Person) {
+            Person p = (Person) obj;
+            return p.name.equals(name);
+        }
+        else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return name.compareTo(o.name);
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<Person> persons = new PriorityQueue<>();
+        persons.add(new Person("ze"));
+        persons.add(new Person("maria"));
+        persons.add(new Person("joaquim"));
+        persons.add(new Person("palerma"));
+        persons.add(new Person("ana"));
+        persons.add(new Person("gustavo"));
+        persons.add(new Person("filipe"));
+
+        System.out.println(persons.remove(new Person("joaquim")));
+        while (persons.size() > 0) {
+            System.out.println(persons.remove());
+        }
+    }
+}
---
+++ src/PriorityQueue.java
@@ -0,0 +1,106 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ricardo on 30/03/2015.
+ */
+public class PriorityQueue<T extends Comparable<T>> {
+    private ArrayList<T> queue = new ArrayList<>();
+    private Map<T, Integer> positions = new HashMap<>();
+    private int nextIndex = 1;
+
+    public PriorityQueue() {
+        queue.add(null);
+    }
+
+
+    public void add(T e) {
+        queue.add(e);
+        positions.put(e, nextIndex);
+        update(nextIndex);
+        nextIndex++;
+    }
+
+    public T remove() {
+        if (nextIndex == 1) return null;
+        swap(1, nextIndex-1);
+        T elem = queue.remove(--nextIndex);
+        update(1);
+        return elem;
+    }
+
+
+    public T remove(T e) {
+        if (nextIndex == 1) return null;
+        int pos = positions.get(e);
+        swap(pos, nextIndex-1);
+        T elem = queue.remove(--nextIndex);
+        update(pos);
+        return elem;
+    }
+
+
+    public int size() {
+        return nextIndex-1;
+    }
+
+    public String toString() {
+        String s = "";
+        for (int i = 1; i < nextIndex; i++) {
+            s += queue.get(i)+"\n";
+        }
+        return s;
+    }
+
+    public void update(int i) {
+        bubbleDown(bubbleUp(i));
+    }
+
+    public void update(T e) {
+        update(positions.get(e));
+    }
+
+    // private methods
+    private int bubbleUp(int i) {
+        int index = i;
+        while (index > 1) {
+            if (queue.get(index).compareTo(queue.get(index / 2)) < 0) {
+                swap(index, index / 2);
+                index /= 2;
+            }
+            else return index;
+        }
+        return i;
+    }
+
+    private void bubbleDown(int i) {
+        int index = i;
+        while (index*2 < nextIndex) {
+            int trade = index*2;
+            int right = index*2+1;
+            if (right < nextIndex) {
+                if (queue.get(trade).compareTo(queue.get(right)) > 0) {
+                    trade = right;
+                }
+            }
+            if (queue.get(index).compareTo(queue.get(trade)) > 0) {
+                swap(index, trade);
+                index = trade;
+            }
+            else return;
+        }
+    }
+
+
+
+
+
+    private void swap(int i, int j) {
+        T tmp = queue.get(i);
+        positions.put(queue.get(j), i);
+        queue.set(i, queue.get(j));
+        positions.put(tmp, j);
+        queue.set(j, tmp);
+    }
+}
